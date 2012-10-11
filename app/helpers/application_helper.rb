af2f9f90a60d6c825140e8bfc2d75ed29047f6bf
Paul Mason <paul@northstarhc.com>
2012-10-11 16:58:17 -0600

    Restrict actions in TicketsController based on permissions and hide links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,12 @@ module ApplicationHelper
     end
   end
   def admins_only(&block)
-  block.call if current_user.try(:admin?)
-  nil
+    block.call if current_user.try(:admin?)
+    nil
+  end
+  def authorized?(permission, thing, &block)
+    block.call if can?(permission.to_sym, thing) ||
+             current_user.try(:admin?)
+    nil
   end
 end
